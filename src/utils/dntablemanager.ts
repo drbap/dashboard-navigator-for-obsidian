a777273658dee1a9790e7cba6845c04ef583c9c6
Bernardo Pires <70039037+drbap@users.noreply.github.com>
2025-07-29 18:42:03 -0300

    Version 7.0.0

--- src/utils/dntablemanager.ts
+++ src/utils/dntablemanager.ts
@@ -1,26 +1,38 @@
 export class DNTableManager {
-	private table: HTMLTableElement;
+	private table: HTMLTableElement | null = null;
 
 	constructor(tbl: string) {
-		this.table = document.querySelector(tbl) as HTMLTableElement;
+		const navTable = document.querySelector(tbl);
+		if (navTable) {
+			this.table = navTable as HTMLTableElement;
+		}
 	}
 
 	private getCells(): NodeListOf<HTMLTableCellElement> {
+		if (!this.table) {
+			return document.createElement('div').querySelectorAll('td, th');
+		}
 		return this.table.querySelectorAll('td, th');
 	}
 
 	hideColumns(columnNames: string[]): void {
+		if (!this.table) {
+			return;
+		}
+
 		const cells = this.getCells();
+		const availableColumnNames = this.getColumnNames();
+		const numColumns = availableColumnNames.length;
 
 		if (columnNames.length === 0) {
 			// Show all columns
-			for (let i = 0; i < cells.length; i++) {
+			for (let i = 0, len = cells.length; i < len; i++) {
 				cells[i].classList.remove('dn-hidden');
 			}
 		} else {
-			for (let i = 0; i < cells.length; i++) {
-				const colIndex = i % 7; // 7 columns of navigator
-				const columnName = this.getColumnNames()[colIndex];
+			for (let i = 0, len = cells.length; i < len; i++) {
+				const colIndex = i % numColumns;
+				const columnName = availableColumnNames[colIndex];
 				if (columnNames.includes(columnName)) {
 					cells[i].classList.add('dn-hidden');
 				} else {
@@ -31,6 +43,6 @@ export class DNTableManager {
 	}
 
 	getColumnNames(): string[] {
-		return ['name', 'ext', 'path', 'size', 'date', 'tags', 'frontmatter'];
+		return ['thumbnail', 'name', 'ext', 'path', 'size', 'date', 'tags', 'frontmatter'];
 	}
 }
