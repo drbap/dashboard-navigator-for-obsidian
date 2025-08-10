2b430a1a5d9b6ab6296c7c4e27e21666d3b83451
Bernardo Pires <70039037+drbap@users.noreply.github.com>
2025-08-10 03:25:16 -0300

    Version 7.1.0

---
+++ src/utils/dnbookmarks.ts
@@ -0,0 +1,45 @@
+export interface BookmarkItem {
+	type: 'file' | 'group';
+	ctime: number;
+	path?: string;
+	items?: BookmarkItem[];
+	title?: string;
+}
+
+export interface BookmarksJson {
+	items: BookmarkItem[];
+}
+
+export function dnGetBookmarkedFiles(bookmarksJson: string): string[] {
+	const bookmarksPaths = new Set<string>();
+	const stack: BookmarkItem[] = [];
+
+	try {
+		const bookmarks: BookmarksJson = JSON.parse(bookmarksJson);
+
+		if (bookmarks.items) {
+			for (let i = bookmarks.items.length - 1; i >= 0; i--) {
+				stack.push(bookmarks.items[i]);
+			}
+		}
+
+		while (stack.length > 0) {
+			const currentItem: BookmarkItem | undefined = stack.pop();
+
+			if (!currentItem) continue;
+
+			if (currentItem.type === 'file' && currentItem.path) {
+				bookmarksPaths.add(currentItem.path);
+			}
+			else if (currentItem.type === 'group' && currentItem.items) {
+				for (let i = currentItem.items.length - 1; i >= 0; i--) {
+					stack.push(currentItem.items[i]);
+				}
+			}
+		}
+	} catch (error) {
+		return [];
+	}
+
+	return Array.from(bookmarksPaths);
+}
